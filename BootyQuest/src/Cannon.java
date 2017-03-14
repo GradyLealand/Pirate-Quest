299bba1e048e1280e4b808b15836aee3429323a7
Grady Williams <kineco@gmail.com>
2017-03-13 22:02:34 -0300

    updated

--- BootyQuest/src/Cannon.java
+++ BootyQuest/src/Cannon.java
@@ -1,6 +1,24 @@
 
-public abstract class Cannon extends Item {
-	private int attackMod = 0;
+public class Cannon extends Item {
+	private int attackMod;
+	private int cannonType;
+	private String cannonName;
+
+	public String getCannonName() {
+		return cannonName;
+	}
+
+	public void setCannonName(String cannonName) {
+		this.cannonName = cannonName;
+	}
+
+	public int getCannonType() {
+		return cannonType;
+	}
+
+	public void setCannonType(int cannonType) {
+		this.cannonType = cannonType;
+	}
 
 	public int getAttackMod() {
 		return attackMod;
@@ -8,5 +26,37 @@ public abstract class Cannon extends Item {
 
 	public void setAttackMod(int attackMod) {
 		this.attackMod = attackMod;
+	}
+	
+	public Cannon cardbordCannon(Cannon cannon){
+		cannon.attackMod = 0;
+		cannon.cannonName = "Cardboard Cannon";
+		cannon.cannonType = 1;
+		cannon.cost = 0;
+		return cannon;
+	}
+	
+	public Cannon copperCannon(Cannon cannon){
+		cannon.attackMod = 1;
+		cannon.cannonName = "Copper Cannon";
+		cannon.cannonType = 2;
+		cannon.cost = 50;
+		return cannon;
+	}
+	
+	public Cannon brassCannon(Cannon cannon){
+		cannon.attackMod = 3;
+		cannon.cannonName = "Brass Cannon";
+		cannon.cannonType = 3;
+		cannon.cost = 250;
+		return cannon;
+	}
+	
+	public Cannon ironCannon(Cannon cannon){
+		cannon.attackMod = 5;
+		cannon.cannonName = "Iron Cannon";
+		cannon.cannonType = 4;
+		cannon.cost = 500;
+		return cannon;
 	}
 }
