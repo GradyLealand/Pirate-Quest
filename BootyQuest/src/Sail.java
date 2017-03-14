14c066412018c0c34359d6c640afd70d6ab08ba8
Grady Williams <kineco@gmail.com>
2017-03-13 22:04:43 -0300

    Update Sail.java

--- BootyQuest/src/Sail.java
+++ BootyQuest/src/Sail.java
@@ -1,6 +1,24 @@
 
-public abstract class Sail extends Item{
-	private int speedMod = 0;
+public class Sail extends Item{
+	private int speedMod;
+	private int sailType;
+	private String sailName;
+
+	public String getSailName() {
+		return sailName;
+	}
+
+	public void setSailName(String sailName) {
+		this.sailName = sailName;
+	}
+
+	public int getSailType() {
+		return sailType;
+	}
+
+	public void setSailType(int sailType) {
+		this.sailType = sailType;
+	}
 
 	public int getSpeedMod() {
 		return speedMod;
@@ -8,6 +26,30 @@ public abstract class Sail extends Item{
 
 	public void setSpeedMod(int speedMod) {
 		this.speedMod = speedMod;
+	}
+	
+	public Sail papperSail(Sail sail){
+		sail.speedMod = 0;
+		sail.sailName = "Papper Sail";
+		sail.sailType = 1;
+		sail.cost = 0;
+		return sail;
+	}
+	
+	public Sail clothSail(Sail sail){
+		sail.speedMod = 1;
+		sail.sailName = "Cloth Sail";
+		sail.sailType = 2;
+		sail.cost = 100;
+		return sail;
+	}
+	
+	public Sail syntheticSail(Sail sail){
+		sail.speedMod = 2;
+		sail.sailName = "Synthetic Sail";
+		sail.sailType = 3;
+		sail.cost = 350;
+		return sail;
 	}
 	
 }
